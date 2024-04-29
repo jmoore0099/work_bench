46acdd938946341ec12576b2919c0215f02dd1e1
jmoore <jeremymoore15@gmail.com>
2024-04-29 08:15:40 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+# jmoore
+# 20240215
+# Module 3.4
+
+# Question 1
+# The difference between "pass" and "continue" is that
+# when a pass statement is used, nothing happens. When a continue statement
+# is used the current layer of the loop is skipped.
+
+# Question 2
+# letter = "Hizthere,zThisziszhowzazpasszstatementzworks!"
+# for i in letter:
+#     if i == "mn":
+#         pass
+#     elif i == "z":
+#         print(" ",end="")
+#     else:
+#         print(i, end="")
+# The program is iterating through the sting character by character.
+# If it finds a character equal to "z" it prints a
+# space(" "), if it doesn't, it prints the character.
+# I'm honestly not sure why the "pass" is in here. Maybe I don't understand "pass" but
+# I'm assuming here that it is just to show that nothing happens? I'm going to try
+# to run the code without the pass line.
+# yeah so, it doesn't seem to change anything.
+
+# Question 3
+
+# print("What shall I wear today?")
+# name = input("Please Enter Your First Name: ")
+# temp = int(input("What is Today's Temperature? "))
+#
+# if temp <= 70:
+#     print(f'Hi {name}, You should probably bring a sweater')
+# else:
+#     print(f'HI {name}, It will be a warm day , T-shirt time!')
+
+# p = lambda n: n**2
+# result = p(4)
+# print(result)
+#
+
