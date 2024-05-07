2d661baad1f674b9ae250db0174576cad033b325
jmoore <jeremymoore15@gmail.com>
2024-05-07 13:36:03 -0700

    mod 4 begun

--- venv/module_4_661.py
+++ venv/module_4_661.py
@@ -1,8 +1,22 @@
 # 20240430
 # jmoore
 import os
+from faker import Faker
 
-print(os.times())
+fake = Faker()
+names = [fake.name() for names in range(50)]
+print(names)
+with open('file.txt', 'w') as file:
+        for line in names:
+            file.write(line)
+
+# Q1
+# Write a code segment that opens a file named file.txt for
+# input and prints the number of lines in that file.
+
+with open('file.txt') as file:
+    for line in file:
+        print(line)
 
 
 
